8d8f6f0d99ae83115e9f9d6e70a469fc1d66b208
KirZaping <wballereau@gmail.com>
2025-01-28 12:27:04 +0100

    pblm de route qui disparait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, RouterOutlet, FooterComponent]
 })
 export class AppComponent {
   title = 'WandrilleIvanaProjet';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HotelByLocationComponent } from './pages/hotel-by-location/hotel-by-location.component';
+import { HotelDescriptionComponent } from './pages/hotel-description/hotel-description.component';
+import { LocationDescriptionComponent } from './pages/location-description/location-description.component';
+import { LocationDiscoverComponent } from './pages/location-discover/location-discover.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'hotel-by-location', component: HotelByLocationComponent },
+  { path: 'hotel-description', component: HotelDescriptionComponent },
+  { path: 'location-description', component: LocationDescriptionComponent },
+  { path: 'location-discover', component: LocationDiscoverComponent },
+  { path: '**', redirectTo: '' }
+];
