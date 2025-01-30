97564e4c86f41ef6826086145a713693c515632b
KirZaping <wballereau@gmail.com>
2025-01-30 16:07:48 +0100

    home updated

--- src/app/components/carrousel/carrousel.component.ts
+++ src/app/components/carrousel/carrousel.component.ts
@@ -1,12 +1,19 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Voyage } from '../../interfaces/voyage';
+
 
 @Component({
   selector: 'app-carrousel',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './carrousel.component.html',
   styleUrl: './carrousel.component.css'
 })
 export class CarrouselComponent {
   @Input() sectionTitle: string = '';
   @Input() id: string = '';
+  @Input() voyages: Voyage[] = [];
+
 }
